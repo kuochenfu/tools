9f0fd0f689b99369ab9376cfc5915153d34afd68
fran <fran@ubnt.com>
2018-04-27 17:18:01 +0800

    Update the scripts

---
+++ ubnt_script/docker-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# installation
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# privilege without sudo, need log out/in.
+sudo usermod -a -G docker $USER
+
+
--- vim_script/vim-install.sh
+++ vim_script/vim-install.sh
@@ -7,8 +7,8 @@ echo "Install the required packages"
 sudo apt-get install -y \
     libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial
-
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
 
 # remove the vim if you have ia already.
 echo "Remove the vim and related pacakges"
@@ -19,16 +19,21 @@ sudo apt-get remove -y vim-tiny vim-common vim-gui-common
 
 #
 echo "Check the source code out and compile"
-cd ~/software
-hg clone https://code.google.com/p/vim/
+git clone https://github.com/vim/vim.git
 cd vim
 ./configure --with-features=huge \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7-config \
-            --enable-perlinterp \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
+							--enable-multibyte \
+							--enable-rubyinterp=yes \
+							--enable-pythoninterp=yes \
+							--with-python-config-dir=/usr/lib/python2.7/config \
+							--enable-python3interp=yes \
+							--with-python3-config-dir=/usr/lib/python3.5/config \
+							--enable-perlinterp=yes \
+							--enable-luainterp=yes \
+							--enable-gui=gtk2 \
+							--enable-cscope \
+							--prefix=/usr/local
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
 sudo make install
 
 # set vim as the default editor.
@@ -40,7 +45,7 @@ sudo update-alternatives --set vi /usr/bin/vim
 
 # get the vundle and install it. (need git tool first)
 # after clone it, we can check the readme in the .vim/bundle/vundle to modify the .vimrc file.
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # install the YCM with Vundle, we have to add the "Bundle 'Valloric/YouCompleteMe'" line to .vimrc
 # After install YCM by Vundle, goto ./vim/bundle/YouCompleteMe folder. Install the YCM by "install.sh".
